f2f9e272272da6db779f894e3449244efcb89279
ArtemVant <93432860+ArtemVant@users.noreply.github.com>
2021-10-30 17:50:38 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1
+    , loop: true
+    , navigation: {
+        nextEl: '.arrow'
+    , },
+      breakpoints: {
+
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    }
+  }
+, });
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
+})
